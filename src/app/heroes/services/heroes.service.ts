9f9ef6b9bdfd2036c89f72bf6fdaaf3d557a9372
sebastian-VB <sebas99peru@gmail.com>
2024-10-22 13:52:59 -0500

    Add new method for search hero by id

--- src/app/heroes/services/heroes.service.ts
+++ src/app/heroes/services/heroes.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { catchError, Observable, of } from 'rxjs';
 import { Hero } from '../interfaces/hero.interface';
 import { environments } from '../../../environments/environments';
 
@@ -15,6 +15,13 @@ export class HeroesService {
 
   getHeroes(): Observable<Hero[]>{
     return this.http.get<Hero[]>(`${this.baseUrl}/heroes`);
+  }
+
+  getHeroById(id: string): Observable<Hero | undefined>{
+    return this.http.get<Hero>(`${this.baseUrl}/heroes/${id}`)
+    .pipe(
+      catchError(err => of(undefined))
+    );
   }
 
 }
