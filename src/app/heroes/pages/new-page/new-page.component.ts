88dca88400692bf8e825f2dae9ae5d6df8aacb19
sebastian-VB <sebas99peru@gmail.com>
2024-10-24 17:06:54 -0500

    Add onInit method for assign values to formgroup

--- src/app/heroes/pages/new-page/new-page.component.ts
+++ src/app/heroes/pages/new-page/new-page.component.ts
@@ -1,14 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
 import { Hero, Publisher } from '../../interfaces/hero.interface';
 import { HeroesService } from '../../services/heroes.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { switchMap } from 'rxjs';
 
 @Component({
   selector: 'app-new-page',
   templateUrl: './new-page.component.html',
   styles: ``
 })
-export class NewPageComponent {
+export class NewPageComponent implements OnInit{
 
   public heroForm = new FormGroup({
     id: new FormControl(''),
@@ -25,7 +27,30 @@ export class NewPageComponent {
     {id: 'Marvel Comics', desc: 'Marvel - Comics'},
   ];
 
-  constructor(private heroesSvc: HeroesService){}
+  constructor(
+    private heroesSvc: HeroesService,
+    private activatedRoute: ActivatedRoute,
+    private router: Router
+  ){}
+
+  ngOnInit(): void {
+
+    if(!this.router.url.includes('edit')) return;
+
+    this.activatedRoute.params
+      .pipe(
+        switchMap(({id}) => this.heroesSvc.getHeroById(id))
+      )
+      .subscribe(hero => {
+
+        if(!hero) return this.router.navigateByUrl('/');
+
+        this.heroForm.reset(hero);
+        return;
+
+      });
+
+  }
 
   get currentHero(): Hero{
     const hero = this.heroForm.value as Hero;
